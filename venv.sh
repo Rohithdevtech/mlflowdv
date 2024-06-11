22eafc49d435433a0773980a14914d54026ca6eb
Rohithdevtech <136415141+Rohithdevtech@users.noreply.github.com>
2024-06-11 10:25:15 +0530

    Create venv.sh

---
+++ venv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the path for the virtual environment
+VENV_PATH="/home/niyata/.jenkins/workspace/mltest22/venv"
+
+# Create the virtual environment
+python3 -m venv $VENV_PATH
+
+# Activate the virtual environment
+source $VENV_PATH/bin/activate
+
+# Optional: Print a message indicating the virtual environment is activated
+echo "Virtual environment activated at $VENV_PATH"
