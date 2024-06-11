d61f38be9af715c46240c2fde4a7593e0973e73f
Rohithdevtech <136415141+Rohithdevtech@users.noreply.github.com>
2024-06-11 10:35:41 +0530

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the path for the virtual environment
-VENV_PATH="/home/niyata/.jenkins/workspace/mltest22/venv"
+VENV_PATH="/home/niyata/.jenkins/workspace/test/venv"
 
 # Create the virtual environment
 python3 -m venv $VENV_PATH
